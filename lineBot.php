6379aeb1ae78421f58ba833f54016ddaaecaa279
mitr900 <44660576+mitr900@users.noreply.github.com>
2019-11-12 20:10:57 +0700

    Update lineBot.php

--- lineBot.php
+++ lineBot.php
@@ -1,156 +1,49 @@
-
 <?php
-	
-//	CREATE BY NONTACHAI KORNINAI
-//	01 OCTOBER 2016
-//
-//	UPDATE BY NONTACHAI KORNINAI
-//	25 MARCH 2018
-	
-require_once __DIR__ . '/setting.php';
-class Linebot {
-	private $channelAccessToken;
-	private $channelSecret;
-	private $webhookResponse;
-	private $webhookEventObject;
-	private $apiReply;
-	private $apiPush;
-	
-	public function __construct(){
-		$this->channelAccessToken = Setting::getChannelAccessToken();
-		$this->channelSecret = Setting::getChannelSecret();
-		$this->apiReply = Setting::getApiReply();
-		$this->apiPush = Setting::getApiPush();
-		$this->webhookResponse = file_get_contents('php://input');
-		$this->webhookEventObject = json_decode($this->webhookResponse);
-	}
-	
-	private function httpPost($api,$body){
-		$ch = curl_init($api); 
-		curl_setopt($ch, CURLOPT_POST, true); 
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST'); 
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($body)); 
-		curl_setopt($ch, CURLOPT_HTTPHEADER, array( 
-		'Content-Type: application/json; charser=UTF-8', 
-		'Authorization: Bearer '.$this->channelAccessToken)); 
-		$result = curl_exec($ch); 
-		curl_close($ch); 
-		return $result;
-	}
-	
-	public function reply($text){
-		$api = $this->apiReply;
-		$webhook = $this->webhookEventObject;
-		$replyToken = $webhook->{"events"}[0]->{"replyToken"}; 
-		$body["replyToken"] = $replyToken;
-		$body["messages"][0] = array(
-			"type" => "text",
-			"text"=>$text
-		);
-		
-		$result = $this->httpPost($api,$body);
-		return $result;
-	}
-	
-	public function push($body){
-		$api = $this->apiPush;
-		$result = $this->httpPost($api, $body);
-		return $result;
-    	}
+ 
+$strAccessToken = "JY/Os8/kUyIEXBkEa+swp35bw8E8z8AvHP7pYD52/9CfNl95HH7Vn6/Yq0aH5pMechgiN/+3RGqOrYPWrKF3mOBUgVPl3DGHlSeNr0ENuTbERigthGzGGJqc78iuRfdOjJJTbkQK4bEuUpCfX+JuHgdB04t89/1O/w1cDnyilFU=";
+ 
+$content = file_get_contents('php://input');
+$arrJson = json_decode($content, true);
+ 
+$strUrl = "https://api.line.me/v2/bot/message/reply";
+ 
+$arrHeader = array();
+$arrHeader[] = "Content-Type: application/json";
+$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+ 
+if($arrJson['events'][0]['message']['text'] == "hi"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "สวัสดี User ID คุณคือ ".$arrJson['events'][0]['source']['userId']. "กรุณาคัดลอกข้อความนี้ ส่งมาที่ ไลน์ ส่วนตัวของ มิตร นะครับเพื่อลงทะเบียน";
 
-    	public function pushText($to, $text){
-		$body = array(
-		    'to' => $to,
-		    'messages' => [
-			array(
-			    'type' => 'text',
-			    'text' => $text
-			)
-		    ]
-		);
-		$this->push($body);
-	 }
-
-   	 public function pushImage($to, $imageUrl, $previewImageUrl = false){
-        	$body = array(
-		    'to' => $to,
-		    'messages' => [
-			array(
-			    'type' => 'image',
-			    'originalContentUrl' => $imageUrl,
-			    'previewImageUrl' => $previewImageUrl ? $previewImageUrl : $imageUrl
-			)
-		    ]
-		);
-		$this->push($body);
-    	}
-
-    	public function pushVideo($to, $videoUrl, $previewImageUrl){
-        	$body = array(
-          	  'to' => $to,
-          	  'messages' => [
-          	      array(
-			    'type' => 'video',
-			    'originalContentUrl' => $videoUrl,
-			    'previewImageUrl' => $previewImageUrl
-			)
-		    ]
-		);
-        	$this->push($body);
-    	}
-
-    	public function pushAudio($to, $audioUrl, $duration){
-		$body = array(
-		    'to' => $to,
-		    'messages' => [
-			array(
-			    'type' => 'audio',
-			    'originalContentUrl' => $audioUrl,
-			    'duration' => $duration
-			)
-		    ]
-		);
-		$this->push($body);
-	}
-
-    	public function pushLocation($to, $title, $address, $latitude, $longitude){
-		$body = array(
-		    'to' => $to,
-		    'messages' => [
-			array(
-			    'type' => 'location',
-			    'title' => $title,
-			    'address' => $address,
-			    'latitude' => $latitude,
-			    'longitude' => $longitude
-			)
-		    ]
-		);
-		$this->push($body);
-	}
-	
-	public function getMessageText(){
-		$webhook = $this->webhookEventObject;
-		$messageText = $webhook->{"events"}[0]->{"message"}->{"text"}; 
-		return $messageText;
-	}
-	
-	public function postbackEvent(){
-		$webhook = $this->webhookEventObject;
-		$postback = $webhook->{"events"}[0]->{"postback"}->{"data"}; 
-		return $postback;
-	}
-	
-	public function getUserId(){
-		$webhook = $this->webhookEventObject;
-		$userId = $webhook->{"events"}[0]->{"source"}->{"userId"}; 
-		return $userId;
-	}
-	public function getGroupId(){
-		$webhook = $this->webhookEventObject;
-		$groupId = $webhook->{"events"}[0]->{"source"}->{"groupId"}; 
-		return $groupId;
-	}
-	
+}else if($arrJson['events'][0]['message']['text'] == "ชื่ออะไร"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "ฉันยังไม่มีชื่อนะ";
+}else if($arrJson['events'][0]['message']['text'] == "ทำอะไรได้บ้าง"){
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "ฉันทำอะไรไม่ได้เลย คุณต้องสอนฉันอีกเยอะ";
+}else{
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = "สวัสดี User ID คุณคือ ".$arrJson['events'][0]['source']['userId']. "กรุณาคัดลอกข้อความนี้ ส่งมาที่ ไลน์ ส่วนตัวของ มิตร นะครับเพื่อลงทะเบียน";
 }
+ 
+ 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,$strUrl);
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+$result = curl_exec($ch);
+curl_close ($ch);
+ 
+?>
